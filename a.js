69cf26cf62c4c3eaf5db64f5cec73ac8cf56db8b
luosheng <luosheng@inspurworld.com>
2020-05-10 15:28:32 +0800

    提交a和c

--- a.js
+++ a.js
@@ -1,3 +1,30 @@
 export default (params) => {
     return !params
 }
+
+
+import React, { Component } from 'react'
+
+
+import React, { Component } from 'react'
+
+export default class a extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+import React, { memo } from 'react'
+
+export default memo(function a() {
+    return (
+        <div>
+            
+        </div>
+    )
+})
---
+++ c.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+export default memo(function c() {
+    return (
+        <div>
+            
+        </div>
+    )
+})
